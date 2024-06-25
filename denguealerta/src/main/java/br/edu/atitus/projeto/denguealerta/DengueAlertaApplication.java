5a629bc90391c5f4fa3694fa571fe5d6ba67f01e
Icarus <fodab1448@gmail.com>
2024-06-25 04:40:41 -0300

    second commit

--- denguealerta/src/main/java/br/edu/atitus/projeto/denguealerta/DengueAlertaApplication.java
+++ denguealerta/src/main/java/br/edu/atitus/projeto/denguealerta/DengueAlertaApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DengueAlertaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DengueAlertaApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DengueAlertaApplication.class, args);
+    }
 }
